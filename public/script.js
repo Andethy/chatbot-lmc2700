4ad4c1197f1f027cbee0170755b76d985ff556e2
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-11-03 14:15:17 -0400

    Update box

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+var count = 1;
 async function sendMessage() {
     const userInput = document.getElementById('user-input').value;
     if (userInput === '') {
@@ -19,7 +20,6 @@ async function sendMessage() {
     // console.log(data.reply)
     appendMessage(data.reply, 'bot');
 }
-
 async function resetChat() {
     const response = await fetch('http://localhost:3000/chat/reset', {
         method: 'POST',
@@ -42,16 +42,50 @@ function appendMessage(content, sender) {
     const chatBox = document.getElementById('chat-box');
     const messageDiv = document.createElement('div');
     messageDiv.classList.add('message', sender + '-message');
-    // messageDiv.innerHTML += '<p class="message-text">' + content + '</p>';
+    // messageDiv.innerHTML += '<pre>' + content + '</pre>';
     messageDiv.textContent = content;
+    count++;
+    messageDiv.style.zIndex = count + '';
     chatBox.appendChild(messageDiv);
-    chatBox.style.overflowY = 'hidden';
-    chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom
-    chatBox.innerHTML += '<br>';
+    // chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom
     // chatBox.scrollTop = chatBox.scrollHeight;
     // wait a second then scroll to the bottom
+    chatBox.style.transform = 'translateZ(0)';
+
     setTimeout(() => {
-        chatBox.style.overflowY = 'scroll';
+        chatBox.style.transform = 'translateZ(0)';
+        chatBox.style.overflowY = 'auto';
+        chatBox.style.transform = 'translateZ(0)';
+        chatBox.style.display = 'none';
+        chatBox.offsetHeight; // Trigger reflow
+        chatBox.style.display = 'block';
     }, 1000);
+    chatBox.style.transform = 'translateZ(0)';
 
 }
+
+
+// This function checks the visibility based on the scroll position
+function checkVisibility() {
+    const windowHeight = window.innerHeight; // Get the window height
+    const elements = document.querySelectorAll('.dynamic-visibility'); // Get all elements you want to check
+
+    elements.forEach((el) => {
+        const rect = el.getBoundingClientRect(); // Get the position of the element relative to the viewport
+        const top = rect.top;
+        const bottom = rect.bottom;
+
+        // Check if the element is within the viewport
+        if (top < windowHeight && bottom >= 0) {
+            // Element is visible
+            el.style.opacity = '1';
+        } else {
+            // Element is not visible
+            el.style.opacity = '0';
+        }
+    });
+}
+
+// Listen for the scroll event on window
+// window.addEventListener('scroll', checkVisibility);
+document.getElementById('chat-box').addEventListener('scroll', checkVisibility);
